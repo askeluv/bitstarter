6cecf244ac13728b8d2e167822bb47c143310bc1
Alex <alexander.svanevik@gmail.com>
2013-07-08 19:58:20 +0000

    Now reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file = fs.readFileSync('index.html');
+  response.send(file.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
